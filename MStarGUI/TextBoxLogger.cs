e727bf7dbe4af9482bf41bcc610dfecce3a8271a
92VoronchikhinAE <VoronchikhinAE01@cbr.ru>
2021-02-14 16:43:21 +0300

    Удалил диалоги распаковки-упаковки, добавил класс TextBoxLogger.

---
+++ MStarGUI/TextBoxLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MStarGUI
+{
+    class TextBoxLogger : IMessageLogger
+    {
+        readonly TextBox LoggerTextBox;
+
+        public TextBoxLogger (TextBox loggerTextBox)
+        {
+            LoggerTextBox = loggerTextBox;
+        }
+
+        public void logMessage (string message)
+        {
+            LoggerTextBox.AppendText( message + "\r\n" );
+        }
+    }
+}
